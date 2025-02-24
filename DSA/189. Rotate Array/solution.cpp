23ba86940852ca1624363eb62d49af7c95dc9496
rashedul <rashedul.islam@dohatec.com.bd>
2025-02-24 12:10:24 +0600

    description added

--- DSA/189. Rotate Array/solution.cpp
+++ DSA/189. Rotate Array/solution.cpp
@@ -1,3 +1,16 @@
+// Author : Rashedul Islam
+// Date   : 2025-02-26
+
+/**********************************************************************************
+ *
+ * Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ *
+ * For this problem, the following formula is applied (current_index + steps) modulo length of array
+ * 
+ * time complexity O(n)
+ * space complexity O(n)
+ **********************************************************************************/
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
