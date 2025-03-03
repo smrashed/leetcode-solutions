1337be9690596fa0f727f563d213a3785c15d83f
smrashed <md.riadcse@gmail.com>
2025-03-03 10:30:17 +0600

    find the index of the first occurrence in a string

---
+++ DSA/28. Find the Index of the First Occurrence in a String/solution.cpp
@@ -0,0 +1,37 @@
+//naive approach
+//time complexity O(n^2)
+
+class Solution
+{
+public:
+    int strStr(string haystack, string needle)
+    {
+        int l1 = haystack.size();
+        int l2 = needle.size();
+
+        for (int i = 0; i < l1;)
+        {
+            if (haystack[i] == needle[0])
+            {
+                int j;
+                int pos = i;
+                for (j = 0; j < l2 && i < l1; j++)
+                {
+                    if (haystack[i] != needle[j])
+                    {
+                        break;
+                    }
+                    i++;
+                }
+                if (j == l2)
+                {
+                    return pos;
+                }
+                i = pos + 1;
+            }
+            else
+                i++;
+        }
+        return -1;
+    }
+};
