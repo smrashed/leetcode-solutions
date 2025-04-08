d617bdf44313a0a64857046092f64cae98d83bd5
smrashed <md.riadcse@gmail.com>
2025-04-08 14:29:10 +0600

    valid parenthesis

---
+++ DSA/20. Valid Parentheses/solution.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<int> stk;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                stk.push(s[i]);
+            }
+            else
+            {
+                if (!stk.empty())
+                {
+                    if (s[i] == ')' && stk.top() == '(')
+                    {
+                        stk.pop();
+                    }
+                    else if (s[i] == '}' && stk.top() == '{')
+                    {
+                        stk.pop();
+                    }
+                    else if (s[i] == ']' && stk.top() == '[')
+                    {
+                        stk.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return stk.empty() ? true : false;
+    }
+};
